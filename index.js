b22a18d9723c9fd8a39168c3562391e9910b074f
dwikisurya <dwikisurya15@gmail.com>
2021-11-02 10:11:40 +0700

    5

--- index.js
+++ index.js
@@ -24,5 +24,5 @@ app.use(json())
 app.use(urlencoded({extended: true}))
 app.use(router)
 
-var port_number = server.listen(process.env.PORT || 3000);
-app.listen(port_number);
+var port_number = server.listen(process.env.PORT || 5000);
+app.listen(port_number, '0.0.0.0');
